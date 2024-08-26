a7e521a7b2d2143b69a7f35ef1aa88fbbab0fb99
Israilla <israillacaroline@hotmail.com>
2024-08-26 16:56:10 -0300

    Refatoração (Dados): Criação do contexto e DbSet's de Fundos e TipoFundos

---
+++ CaseItau.Infraestrutura/Dados/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using CaseItau.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CaseItau.Infraestrutura.Dados
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options)
+        {
+        }
+        public DbSet<Fundo> Fundos { get; set; }
+        public DbSet<TipoFundo> TiposFundos { get; set; }
+    }
+}
