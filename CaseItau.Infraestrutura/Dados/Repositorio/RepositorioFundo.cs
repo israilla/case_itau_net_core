cfc949a80b1c942494511f23ac3323e926b0831b
Israilla <israillacaroline@hotmail.com>
2024-08-26 16:59:28 -0300

    Refatoração (Repositório): Criação do Repositório de Fundos

---
+++ CaseItau.Infraestrutura/Dados/Repositorio/RepositorioFundo.cs
@@ -0,0 +1,22 @@
+﻿using CaseItau.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaseItau.Infraestrutura.Dados.Repositorio
+{
+    public class RepositorioFundo 
+    {
+        private readonly Contexto _contexto;
+
+        public RepositorioFundo(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+        
+        public IEnumerable<Fundo> ObterTodos()
+        {
+            return _contexto.Fundos
+            .Include(f => f.TipoFundo)
+            .ToList();
+        }
+    }
+}
