a19d03203af8e13a718df44eac1a0dd926bd2654
Israilla <israillacaroline@hotmail.com>
2024-08-26 17:10:52 -0300

    Refatoração (Serviço): Criando serviço de fundos

---
+++ CaseItau.Dominio/Servicos/ServicoFundo.cs
@@ -0,0 +1,19 @@
+﻿using CaseItau.Dominio.Entidades;
+using CaseItau.Dominio.Interfaces;
+
+namespace CaseItau.Dominio.Servicos
+{
+    public class ServicoFundo
+    {
+        private readonly IRepositorioFundo _repositorioFundo;
+
+        public ServicoFundo(IRepositorioFundo repositorioFundo)
+        {
+                _repositorioFundo = repositorioFundo;   
+        }
+        public IEnumerable<Fundo> ObterTodos()
+        {
+            return _repositorioFundo.ObterTodos();
+        }
+    }
+}
