f0076ab9012e8a68c363d397d4d470249cb7917f
Israilla <israillacaroline@hotmail.com>
2024-08-26 23:30:01 -0300

    Refatoração (Interface): Criação da interface IRepositorioFundo

---
+++ CaseItau.Dominio/Interfaces/Repositorios/IRepositorioFundo.cs
@@ -0,0 +1,14 @@
+﻿using CaseItau.Dominio.Entidades;
+
+namespace CaseItau.Dominio.Interfaces.Repositorios
+{
+    public interface IRepositorioFundo
+    {
+        IEnumerable<Fundo> ObterTodos();
+        Task<Fundo> ObterFundoPorCodigo(string codigo);
+        Task IncluirFundo(Fundo fundo);
+        Task AlterarFundo(Fundo fundo);
+        Task ExcluirFundo(string codigo);
+        Task AtualizarPatrimonio(string codigo, decimal valor);
+    }
+}
