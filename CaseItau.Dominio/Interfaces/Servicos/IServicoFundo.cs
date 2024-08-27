682633cb4a3574f7453fe86c5ddab0d7b91b0d8d
Israilla <israillacaroline@hotmail.com>
2024-08-27 16:07:08 -0300

    Refatoração (DTO): Ajuste nos métodos para utilização do DTO

--- CaseItau.Dominio/Interfaces/Servicos/IServicoFundo.cs
+++ CaseItau.Dominio/Interfaces/Servicos/IServicoFundo.cs
@@ -1,19 +1,16 @@
-﻿using CaseItau.Dominio.Entidades;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CaseItau.Dominio.Dto;
+using CaseItau.Dominio.Entidades;
 
 namespace CaseItau.Dominio.Interfaces.Servicos
 {
     public interface IServicoFundo
     {
-        IEnumerable<Fundo> ObterTodos();
-        Task<Fundo> ObterFundoPorCodigo(string codigo);
-        Task IncluirFundo(Fundo fundo);
-        Task AlterarFundo(Fundo fundo);
+        IEnumerable<FundoDto> ObterTodos();
+        Task<FundoDto> ObterFundoPorCodigo(string codigo);
+        Task IncluirFundo(FundoDto fundo);
+        Task AlterarFundo(FundoDto fundo);
         Task ExcluirFundo(string codigo);
         Task AtualizarPatrimonio(string codigo, decimal valor);
+        Task<Fundo> ObterFundoPorCnpj(string cnpj);
     }
 }
--- CaseItau.Dominio/Servicos/ServicoFundo.cs
+++ CaseItau.Dominio/Servicos/ServicoFundo.cs
@@ -1,4 +1,5 @@
-﻿using CaseItau.Dominio.Entidades;
+﻿using CaseItau.Dominio.Dto;
+using CaseItau.Dominio.Entidades;
 using CaseItau.Dominio.Interfaces.Repositorios;
 using CaseItau.Dominio.Interfaces.Servicos;
 
@@ -12,21 +13,60 @@ namespace CaseItau.Dominio.Servicos
         {
                 _repositorioFundo = repositorioFundo;   
         }
-        public IEnumerable<Fundo> ObterTodos()
+        public IEnumerable<FundoDto> ObterTodos()
         {
-            return _repositorioFundo.ObterTodos();
+             var fundos = _repositorioFundo.ObterTodos();
+            
+            return fundos.Select(f => new FundoDto
+            {
+                Codigo = f.Codigo,
+                Nome = f.Nome,
+                Patrimonio = f.Patrimonio,
+                CodigoTipo = f.Codigo_Tipo,
+                NomeTipo = f.Tipo_Fundo.Nome
+            }).ToList();
         }
-        public Task<Fundo> ObterFundoPorCodigo(string codigo)
+        public async Task<FundoDto> ObterFundoPorCodigo(string codigo)
         {
-            return _repositorioFundo.ObterFundoPorCodigo(codigo);
+             var fundo = await _repositorioFundo.ObterFundoPorCodigo(codigo);
+
+            if (fundo == null) return null;
+
+            return new FundoDto
+            {
+                Codigo = fundo.Codigo,
+                Nome = fundo.Nome,
+                Patrimonio = fundo.Patrimonio,
+                CodigoTipo = fundo.Codigo_Tipo,
+                NomeTipo = fundo.Tipo_Fundo.Nome,
+                Cnpj = fundo.Cnpj,
+            };
         }
-        public async Task IncluirFundo(Fundo fundo)
+        public async Task IncluirFundo(FundoDto fundo)
         {
-            await _repositorioFundo.IncluirFundo(fundo);
+            var novoFundo = new Fundo
+            {
+                Codigo = fundo.Codigo,
+                Nome = fundo.Nome,
+                Patrimonio = fundo.Patrimonio,
+                Codigo_Tipo = fundo.CodigoTipo,
+                Cnpj = fundo.Cnpj
+            };
+
+            await _repositorioFundo.IncluirFundo(novoFundo);
         }
-        public async Task AlterarFundo(Fundo fundo)
+        public async Task AlterarFundo(FundoDto fundo)
         {
-            await _repositorioFundo.AlterarFundo(fundo);
+            var fundoAlterado = new Fundo
+            {
+                Codigo = fundo.Codigo,
+                Nome = fundo.Nome,
+                Patrimonio = fundo.Patrimonio,
+                Codigo_Tipo = fundo.CodigoTipo,
+                Cnpj = fundo.Cnpj
+            };
+
+            await _repositorioFundo.AlterarFundo(fundoAlterado);
         }
         public async Task ExcluirFundo(string codigo)
         {
@@ -36,6 +76,11 @@ namespace CaseItau.Dominio.Servicos
         {
             await _repositorioFundo.AtualizarPatrimonio(codigo, valor);
         }
+        public async Task<Fundo> ObterFundoPorCnpj(string cnpj)
+        {
+            var fundo = await _repositorioFundo.ObterFundoPorCnpj(cnpj);
 
+            return fundo;
+        }
     }
 }
